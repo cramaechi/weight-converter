59b18f75eac2621507693139b9587ecebb2ac2f4
Chibuikem Amaechi <chibuikem_amaechi@Chibuikems-MacBook-Pro.local>
2017-12-08 11:45:28 -0600

    Modified .main.cpp.swp and main.cpp

--- main.cpp
+++ main.cpp
@@ -19,52 +19,52 @@ void output(double output1, double output2, char& a);
 
 int main()
 {
-	char ans;
-	double p, o, kG, g;
+    char ans;
+    double p, o, kG, g;
 
-	do
-	{
-		input(p, o);
-		convertWeight(p, o, kG, g);
-		output(kG, g, ans);
-		
-		if (ans == 'y' || ans == 'Y')
-			cout<<"\n\n";
+    do
+    {
+        input(p, o);
+        convertWeight(p, o, kG, g);
+        output(kG, g, ans);
 
-	}while(ans == 'y' || ans == 'Y');
+        if (ans == 'y' || ans == 'Y')
+            cout<<"\n\n";
 
-	return 0;
+    }while(ans == 'y' || ans == 'Y');
+
+    return 0;
 }
 
 void input(double& input1, double& input2)
 {
-	cout<<"Enter weight in pounds and ounces: ";
-	cin>>input1>>input2;
-	cout<<endl;
+    cout<<"Enter weight in pounds and ounces: ";
+    cin>>input1>>input2;
+    cout<<endl;
 }
 
 void convertWeight(double pounds, double ounces, double& kilograms, double& grams)
 {
-	pounds+=(ounces/16);
-	kilograms = (pounds/2.2046);
-	grams = 1000*kilograms;
+    pounds+=(ounces/16);
+    kilograms = (pounds/2.2046);
+    grams = 1000*kilograms;
 }
 
 void output(double output1, double output2, char& a)
 {
-	cout.setf(ios::showpoint);
-	cout.setf(ios::fixed);
-	cout.precision(2);
-	cout<<"The equivalent weights in kilograms and grams are\n\n";
-	cout<<output1<<" kg\n";
-	cout<<output2<<" g\n\n";
-	cout<<"Start over? (y/n): ";
-	cin>>a;
+    cout.setf(ios::showpoint);
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    cout<<"The equivalent weights in kilograms and grams are\n\n";
+    cout<<output1<<" kg\n";
+    cout<<output2<<" g\n\n";
+    cout<<"Start over? (y/n): ";
+    cin>>a;
 }
 
 /*
-void clearScreen()
-{
-	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-}
-*/
+   void clearScreen()
+   {
+   cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+   }
+   */
